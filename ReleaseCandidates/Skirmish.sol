2f803747798f535ed54ac9d7cbdce9c1e22b7daf
SoulHarvesterDev <143414089+SoulHarvesterDev@users.noreply.github.com>
2023-09-13 20:04:01 +0200

    Update Skirmish.sol

--- ReleaseCandidates/Skirmish.sol
+++ ReleaseCandidates/Skirmish.sol
@@ -6,20 +6,24 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Skirmish is Ownable, Pausable {
+contract Skirmish is Ownable, Pausable, ReentrancyGuard {
     using SafeERC20 for IERC20;
+
+    address public manager;
+
     IERC20 public token;
 
     uint256 public totalVotes;
-    uint256 public distribution_Vault;
+    uint256 public distribution_Vessel;
     uint256 public distribution_Team;
     uint256 public voting_bonus_OG; 
     uint256 public voteFinishTime;
     uint256 public season;
 
     address public teamAddress;
-    address public vaultAddress;
+    address public vesselAddress;
     address public ogNFTAddress;
     
     bool public votingActive;
@@ -45,7 +49,6 @@ contract Skirmish is Ownable, Pausable {
 
     struct collection{
         uint256 votesApplied;
-        uint256 actualShare;
         bool isNominee;
         bool hasWon;
     }
@@ -57,33 +60,33 @@ contract Skirmish is Ownable, Pausable {
 
     constructor(
         IERC20 _token, 
-        address _vaultAddress, 
+        address _vesselAddress, 
         address _teamAddress, 
-        uint256 _distributionTeam, 
-        uint256 _distributionVault, 
-        address _ogNFTAddress, 
-        uint256 _voting_bonus_OG
+        address _ogNFTAddress 
         ) {
         token = _token;
 
-        distribution_Vault = _distributionVault;    // value/100000
-        distribution_Team = _distributionTeam;      // value/100000
+        distribution_Vessel = 50000;    // value/100000
+        distribution_Team = 30000;      // value/100000
         ogNFTAddress = _ogNFTAddress;               
-        voting_bonus_OG = _voting_bonus_OG;         // value/100000
+        voting_bonus_OG = 110000;         // value/100000
 
-        // distribution_Vault = 50000;          // value/100000 (e.g. 50000/100000 = 50.000%)
+        // distribution_Vessel = 50000;          // value/100000 (e.g. 50000/100000 = 50.000%)
         // distribution_Team = 30000;           // value/100000 (e.g. 30000/100000 = 30.000%)
         // ogNFTAddress = 0x0fE3552D5073A92A9021003042900aFaD3490bd9;
         // voting_bonus_OG = 110000;            // value/100000 (e.g. 110000/100000 = 110.000%)
 
         teamAddress = _teamAddress;
-        vaultAddress = _vaultAddress;
+        vesselAddress = _vesselAddress;
         season = 0;
         resetted = true;
         votingActive = false;
+
+        manager = msg.sender;
+
     } 
 
-    function startVoting() public onlyOwner {
+    function startVoting() public onlyOwnerOrManager {
         require(listOfNominees.length >= 2, "At least 2 Nominees have to be listed!");
         require(resetted, "List of nominees has to get resetted first!");
 
@@ -93,37 +96,58 @@ contract Skirmish is Ownable, Pausable {
         resetted = false;
         votingActive = true;
     }
-    function finishVoting() public onlyOwner {
+    function finishVoting() public onlyOwnerOrManager {
         require(votingActive, "Voting is not live yet!");
         require(block.timestamp >= voteFinishTime, "Voting still running!");
 
+        if (totalVotes > 0){
+            uint256 votingResult = 0;
+            uint256 share;
+            address seasonWinner;
 
-        uint256 votingResult = 0;
-        uint256 share;
-        address seasonWinner;
+            for(uint256 i = 0; i < listOfNominees.length; i++){
 
-        for(uint256 i = 0; i < listOfNominees.length; i++){
+                address nominee = listOfNominees[i];
 
-            address nominee = listOfNominees[i];
-
-            if(votingResult < collectionInformation[nominee].votesApplied){
-                votingResult = collectionInformation[nominee].votesApplied;
-                seasonWinner = nominee;
+                if(votingResult < collectionInformation[nominee].votesApplied){
+                    votingResult = collectionInformation[nominee].votesApplied;
+                    seasonWinner = nominee;
+                }
             }
-        }
-        share = votingResult * 100000 / totalVotes;
-        seasonHistory.push(historyValues(season,seasonWinner,totalVotes,voter.length,share));
-        season++;
-        collectionInformation[seasonWinner].hasWon = true;
-        votingActive = false;
-    }
 
-    function manuallyEndSeason() public onlyOwner {
+            share = votingResult * 100000 / totalVotes;
+            seasonHistory.push(historyValues(season,seasonWinner,totalVotes,voter.length,share));
+            season++;
+            collectionInformation[seasonWinner].hasWon = true;
+            
+            uint256 amount = token.balanceOf(address(this));
+            uint256 tokenForVessel = amount * distribution_Vessel / 100000;
+            uint256 tokenForTeam = amount * distribution_Team / 100000;
+            uint256 tokenForBurn = amount - tokenForTeam - tokenForVessel;
+
+            // Distribute tokens
+            token.safeTransfer(address(0), tokenForBurn);
+            token.safeTransfer(teamAddress, tokenForTeam);
+            token.safeTransfer(vesselAddress, tokenForVessel);        
+
+
+        }
+        for(uint256 i = 0; i < listOfNominees.length; i++){
+            collectionInformation[listOfNominees[i]].votesApplied = 0;
+            collectionInformation[listOfNominees[i]].isNominee = false;
+        }
+
+        delete voter;
+        delete listOfNominees;
+        votingActive = false;
+        resetted = true;
+    }
+    function manuallyEndSeason() public onlyOwnerOrManager {
         require(block.timestamp <= voteFinishTime, "Voting has already ended!");
         voteFinishTime = block.timestamp;
     }
 
-    function voteForNominee(uint256 amount, address nominee) public whenNotPaused{
+    function voteForNominee(uint256 amount, address nominee) public nonReentrant whenNotPaused{
         require(votingActive, "Voting is not live yet!");
         require(collectionInformation[nominee].isNominee, "NFT contract is not a nominee!");
         require(block.timestamp <= voteFinishTime, "Voting has already ended!");
@@ -138,12 +162,7 @@ contract Skirmish is Ownable, Pausable {
         totalVotes += votingPower;
         token.safeTransferFrom(msg.sender, address(this), amount);
         
-
         collectionInformation[nominee].votesApplied += votingPower;
-
-        for(uint256 i = 0; i < listOfNominees.length; i++){
-            collectionInformation[listOfNominees[i]].actualShare = collectionInformation[listOfNominees[i]].votesApplied * 100000 / totalVotes;
-        }
 
         voter.push(activeVoter(msg.sender,nominee,votingPower));
 
@@ -158,7 +177,7 @@ contract Skirmish is Ownable, Pausable {
         emit VotesApplied(msg.sender, votingPower, nominee);
     }
 
-    function addNominee(address nominee) public onlyOwner {
+    function addNominee(address nominee) public onlyOwnerOrManager {
         require(!votingActive, "Voting is already active!");
         require(resetted, "Data from last season are not yet resetted!");
         require(!collectionInformation[nominee].isNominee, "NFT contract is already nominee!");
@@ -166,7 +185,6 @@ contract Skirmish is Ownable, Pausable {
         require(listOfNominees.length < 3, "Already 3 Nominees are listed!");
 
         collectionInformation[nominee].votesApplied = 0;
-        collectionInformation[nominee].actualShare = 0;
         collectionInformation[nominee].isNominee = true;
         listOfNominees.push(nominee);
     }
@@ -196,30 +214,7 @@ contract Skirmish is Ownable, Pausable {
         uint256 share = collectionInformation[_nominee].votesApplied * 100000 / totalVotes;
         return share;
     }
-    function cleanUp() public onlyOwner {
-        uint256 amount = token.balanceOf(address(this));
-        require(!votingActive,"Voting is still active!");
-        require(amount > 0, "Amount must be greater than zero");
-        
-        uint256 tokenForVault = amount * distribution_Vault / 100000;
-        uint256 tokenForTeam = amount * distribution_Team / 100000;
-        uint256 tokenForBurn = amount - tokenForTeam - tokenForVault;
 
-            // Distribute tokens to the caller, team and bribing address
-            token.safeTransfer(address(0), tokenForBurn);
-            token.safeTransfer(teamAddress, tokenForTeam);
-            token.safeTransfer(vaultAddress, tokenForVault);        
-
-        for(uint256 i = 0; i < listOfNominees.length; i++){
-            collectionInformation[listOfNominees[i]].votesApplied = 0;
-            collectionInformation[listOfNominees[i]].actualShare = 0;
-            collectionInformation[listOfNominees[i]].isNominee = false;
-        }
-
-        delete voter;
-        delete listOfNominees;
-        resetted = true;
-    }
 
     function setTokenAddress(address _tokenAddress) public onlyOwner {
         token = IERC20(_tokenAddress);
@@ -233,12 +228,12 @@ contract Skirmish is Ownable, Pausable {
         distribution_Team = _teamPortion;       // value / 100000
     }
 
-    function setVaultAddress(address _vaultAddress) public onlyOwner {
-        vaultAddress = _vaultAddress;
+    function setVesselAddress(address _vesselAddress) public onlyOwner {
+        vesselAddress = _vesselAddress;
     } 
 
-    function setVaultPortion(uint256 _vaultPortion) public onlyOwner {
-        distribution_Vault = _vaultPortion;     // value / 100000
+    function setVesselPortion(uint256 _vesselPortion) public onlyOwner {
+        distribution_Vessel = _vesselPortion;     // value / 100000
     }
 
     function setOGNFTAddress(address _ogNFTAddress) public onlyOwner {
@@ -264,5 +259,14 @@ contract Skirmish is Ownable, Pausable {
     function unpause() public onlyOwner {
         _unpause();
     }
+        
+    modifier onlyOwnerOrManager() {
+    address _owner = owner();
+    require(msg.sender == _owner || msg.sender == manager, "You are neither the owner nor the manager!");
+    _;
+    }    
     
+    function changeManager(address newManager) public onlyOwner {
+    manager = newManager;
+    }
     }
